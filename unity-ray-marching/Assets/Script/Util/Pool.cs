c09be35c3b22f71401d6b758f087e9684e01d263
Allen Chou <minglun.chou@gmail.com>
2020-04-20 17:54:24 -0700

    Pool.

---
+++ unity-ray-marching/Assets/Script/Util/Pool.cs
@@ -0,0 +1,34 @@
+﻿/******************************************************************************/
+/*
+  Project - Unity Ray Marching
+            https://github.com/TheAllenChou/unity-ray-marching
+
+  Author  - Ming-Lun "Allen" Chou
+  Web     - http://AllenChou.net
+  Twitter - @TheAllenChou
+
+*/
+/******************************************************************************/
+
+using System.Collections.Generic;
+
+public class Pool<T> where T : class
+{
+  private static List<T> s_aFreeObj = new List<T>();
+
+  public static T Take()
+  {
+    if (s_aFreeObj.Count == 0)
+      return default(T);
+
+    T obj = s_aFreeObj[s_aFreeObj.Count - 1];
+    s_aFreeObj.RemoveAt(s_aFreeObj.Count - 1);
+
+    return obj;
+  }
+
+  public static void Store(T obj)
+  {
+    s_aFreeObj.Add(obj);
+  }
+}
